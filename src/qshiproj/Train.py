42af8a5a9a18b9bc83ec745dbc603070ce1b2423
qibinshi <35798077+qibinshi@users.noreply.github.com>
2023-04-04 08:43:30 -0700

    add model weights

--- src/qshiproj/Train.py
+++ src/qshiproj/Train.py
@@ -50,7 +50,7 @@ def train(configure_file='config.ini'):
     epochs = config.getint('training', 'epochs')
     minimum_epochs = config.getint('training', 'minimum_epochs')
     patience = config.getint('training', 'patience')
-    pre_trained_denote = pkg_resources.resource_filename(__name__, 'DenoTe.pth')
+    pre_trained_denote = pkg_resources.resource_filename(__name__, 'DenoTe_wt.pth')
     pre_trained_WaveDecompNet = pkg_resources.resource_filename(__name__, 'WaveDecompNet.pth')
 
     print('transfer', transfer)
